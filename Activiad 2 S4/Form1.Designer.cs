16bbc6d366d2e94cf1821e08742f13914e89ddf3
Eduardo Alejandro Martínez Villalobos <eduardoalejandro.v.mtz@gmail.com>
2024-04-28 23:52:45 -0600

    Agregar archivos de proyecto.

---
+++ Activiad 2 S4/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace Activiad_2_S4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnWrite = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.btnRead = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnWrite
+            // 
+            this.btnWrite.Location = new System.Drawing.Point(118, 80);
+            this.btnWrite.Name = "btnWrite";
+            this.btnWrite.Size = new System.Drawing.Size(75, 23);
+            this.btnWrite.TabIndex = 0;
+            this.btnWrite.Text = "Save";
+            this.btnWrite.UseVisualStyleBackColor = true;
+            this.btnWrite.Click += new System.EventHandler(this.btnWrite_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Location = new System.Drawing.Point(87, 25);
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.Size = new System.Drawing.Size(329, 23);
+            this.txtInput.TabIndex = 1;
+            // 
+            // btnRead
+            // 
+            this.btnRead.Location = new System.Drawing.Point(211, 80);
+            this.btnRead.Name = "btnRead";
+            this.btnRead.Size = new System.Drawing.Size(75, 23);
+            this.btnRead.TabIndex = 2;
+            this.btnRead.Text = "Read";
+            this.btnRead.UseVisualStyleBackColor = true;
+            this.btnRead.Click += new System.EventHandler(this.btnRead_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(311, 80);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 3;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(36, 154);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(35, 13);
+            this.lblOutput.TabIndex = 4;
+            this.lblOutput.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 420);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnRead);
+            this.Controls.Add(this.txtInput);
+            this.Controls.Add(this.btnWrite);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnWrite;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.Button btnRead;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
---
+++ Activiad 2 S4/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Activiad_2_S4
+{
+    public partial class Form1 : Form
+    {
+
+        // Ruta del archivo secuencial
+        private const string filePath = "sequentialFile.txt";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnWrite_Click(object sender, EventArgs e)
+        {
+            // Take the text from textbox
+            string textToWrite = txtInput.Text;
+
+            // Open the file in mode append and write the text
+            using (StreamWriter writer = new StreamWriter(filePath, true))
+            {
+                writer.WriteLine(textToWrite);
+            }
+
+            // Clear TextBox
+            txtInput.Clear();
+
+            lblOutput.Text = "Text written to file.";
+        }
+
+        private void btnRead_Click(object sender, EventArgs e)
+        {
+            // Veroify if the file exists
+            if (File.Exists(filePath))
+            {
+                // Read all the content of the file
+                using (StreamReader reader = new StreamReader(filePath))
+                {
+                    string content = reader.ReadToEnd();
+                    lblOutput.Text = "File content:\n" + content;
+                }
+            }
+            else
+            {
+                lblOutput.Text = "File does not exist.";
+            }
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            // Verify if the file exists
+            if (File.Exists(filePath))
+            {
+                //  Delete the file
+                File.Delete(filePath);
+                lblOutput.Text = "File deleted.";
+            }
+            else
+            {
+                lblOutput.Text = "File does not exist.";
+            }
+        }
+    }
+}
